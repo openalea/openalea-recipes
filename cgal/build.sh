c7f3797dc1624e228e106ca684dd8a339fe9d0f8
pradal <christophe.pradal@inria.fr>
2016-01-07 17:39:47 +0100

    Build cgal on Mac

---
+++ cgal/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# setting GMP_LIBRARIES with the full -L<path> -lgmp
+# was necessary on OSX/clang.  Not sure if it somehow worked on linux
+# with just the path
+# had been using SYS_PREFIX, but PREFIX is probably more appropriate since
+# having gmp as a requirement will install it to PREFIX, not SYS_PREFIX
+# from https://github.com/rustychris/conda-recipes/
+
+cmake -DGMP_LIBRARIES="-L$PREFIX/lib -lgmp" -DGMP_INCLUDE_DIR=$PREFIX/include -DCMAKE_INSTALL_PREFIX=$PREFIX .
+make
+make install
