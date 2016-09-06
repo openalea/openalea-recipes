bd937019408363f97c7897ce22a4b4159716597a
pradal <christophe.pradal@inria.fr>
2016-09-06 10:01:50 +0200

    Update conda management with sconsx

--- vplants.tool/build.sh
+++ vplants.tool/build.sh
@@ -5,6 +5,4 @@ cd tool
 #$PYTHON setup.py build_ext --scons-ext-param="QTDIR=$PREFIX QT4_BINPATH=$PREFIX/bin QT4_CPPPATH=$PREFIX/include QT4_LIBPATH=$PREFIX/lib QT4_FRAMEWORK=False" \
 #                 install --prefix=$PREFIX
 
-scons QTDIR=$PREFIX QT4_BINPATH=$PREFIX/bin QT4_CPPPATH=$PREFIX/include QT4_LIBPATH=$PREFIX/lib QT4_FRAMEWORK=False build
-cp -R build-scons/lib $PREFIX
-cp -R build-scons/include $PREFIX
+scons QTDIR=$PREFIX QT4_FRAMEWORK=False QT4_BINPATH=$PREFIX/bin QT4_LIBPATH=$PREFIX/lib QT4_CPPPATH=$PREFIX/include/qt build_prefix=$PREFIX build_includedir=$PREFIX/include build_libdir=$PREFIX/lib build_bindir=$PREFIX/bin build
