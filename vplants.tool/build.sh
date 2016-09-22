1a7dbf97c86f048ef8b60e89d8eac0db7c15bdb7
pradal <christophe.pradal@inria.fr>
2016-09-22 14:34:54 +0200

    Update vplants.tool recipes

--- vplants.tool/build.sh
+++ vplants.tool/build.sh
@@ -5,4 +5,6 @@ cd tool
 #$PYTHON setup.py build_ext --scons-ext-param="QTDIR=$PREFIX QT4_BINPATH=$PREFIX/bin QT4_CPPPATH=$PREFIX/include QT4_LIBPATH=$PREFIX/lib QT4_FRAMEWORK=False" \
 #                 install --prefix=$PREFIX
 
-scons QTDIR=$PREFIX QT4_FRAMEWORK=False QT4_BINPATH=$PREFIX/bin QT4_LIBPATH=$PREFIX/lib QT4_CPPPATH=$PREFIX/include/qt build_prefix=$PREFIX build_includedir=$PREFIX/include build_libdir=$PREFIX/lib build_bindir=$PREFIX/bin build
+scons QTDIR=$PREFIX QT4_BINPATH=$PREFIX/bin QT4_CPPPATH=$PREFIX/include QT4_LIBPATH=$PREFIX/lib QT4_FRAMEWORK=False \
+build_prefix=$PREFIX build_includedir=$PREFIX/include build_libdir=$PREFIX/lib build_bindir=$PREFIX/bin \
+readline_includes=$PREFIX/include readline_lib=$PREFIX/lib termcap_includes=$PREFIX/include termcap_lib=$PREFIX/lib EXTRA_LINKFLAGS='-lreadline.6' build
