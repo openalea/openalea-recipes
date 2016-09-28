35e9791463f8092bd237d2b43af2281625844d4f
pradal <christophe.pradal@inria.fr>
2016-09-28 17:11:50 +0200

    New recipe for PyQGLViewer

---
+++ pyqglviewer/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+set -x -e
+
+if [ "$(uname)" == "Darwin" ];
+then
+    export CC=clang
+    export CXX=clang++
+
+    export MACOSX_VERSION_MIN=10.7
+    export CXXFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
+    export CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
+    export LINKFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
+    export LINKFLAGS="${LINKFLAGS} -stdlib=libc++ -std=c++11 "
+fi
+
+export QMAKESPEC=macx-g++
+
+python configure.py -I $PREFIX/include \
+-I $PREFIX/include/Qt -I $PREFIX/include/QtCore -I $PREFIX/include/QtOpenGL -I $PREFIX/include/QtXml -I $PREFIX/include/QtGUI \
+-L $PREFIX/lib --extra-cxxflags="${CXXFLAGS}" --extra-lflags="${LINKFLAGS}"
+
+
+make
+make install
