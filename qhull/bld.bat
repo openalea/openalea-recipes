1dc1b89d3da052a2f6e67471b5be306fc46a5ec6
Simon Artzet <simon.artzet@gmail.com>
2017-11-28 12:02:50 +0100

    update bld.bat procedure for windows

--- qhull/bld.bat
+++ qhull/bld.bat
@@ -1,7 +1,6 @@
-copy bin\q*.exe "%LIBRARY_BIN%" || exit /b 1
-copy bin\rbox.exe "%LIBRARY_BIN%" || exit /b 1
-copy bin\q*.dll "%LIBRARY_BIN%" || exit /b 1
-
-mkdir "%LIBRARY_PREFIX%\share\doc\%PKG_NAME%"
-copy COPYING.txt "%LIBRARY_PREFIX%\share\doc\%PKG_NAME%" || exit /b 1
-
+mkdir -p build-cmake
+cd build-cmake
+cmake -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=%PREFIX% ..
+cmake ..
+nmake
+nmake install
