3259b43fccca3a1dc2f77bd6ede948303a8739f5
Frederic Boudon <frederic.boudon@cirad.fr>
2017-11-28 16:53:24 +0100

    compile qhull in release mode. Add information.

--- qhull/bld.bat
+++ qhull/bld.bat
@@ -1,6 +1,6 @@
 mkdir -p build-cmake
 cd build-cmake
-cmake -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ..
+cmake -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DCMAKE_BUILD_TYPE=Release ..
 cmake ..
 nmake
 nmake install
