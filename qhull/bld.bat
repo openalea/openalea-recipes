23173d086780f66bbcb0d0eec063bbb3945f95e2
Frederic Boudon <frederic.boudon@cirad.fr>
2017-11-28 15:16:25 +0100

    update qhull to use version 2015.2

--- qhull/bld.bat
+++ qhull/bld.bat
@@ -1,6 +1,6 @@
 mkdir -p build-cmake
 cd build-cmake
-cmake -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=%PREFIX% ..
+cmake -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ..
 cmake ..
 nmake
 nmake install
