e7db76aa1c8e292329088ca062ab20b49ee70ed4
Christophe Pradal <christophe.pradal@inria.fr>
2018-01-31 18:39:18 +0100

    Update bld.bat

--- boost/bld.bat
+++ boost/bld.bat
@@ -1,7 +1,19 @@
 :: Start with bootstrap
-call bootstrap.bat vc%VS_MAJOR%
+:: Set the right msvc version according to Python version
+if "%PY_VER%"=="2.7" (
+    set MSVC_VER=9.0
+    set LIB_VER=90
+) else if "%PY_VER%"=="3.4" (
+    set MSVC_VER=10.0
+    set LIB_VER=100
+) else (
+    set MSVC_VER=14.0
+    set LIB_VER=140
+)
 
-if errorlevel 1 exit /b 1
+call bootstrap.bat
+
+if errorlevel 1 exit 1
 
 set LogFile=b2.build.log
 set TempLog=b2.build.log.tmp
@@ -22,4 +34,10 @@ set LogTee=^> %TempLog%^&^& type %TempLog%^&^&type %TempLog%^>^>%LogFile%
   stage ^
   %LogTee%
 
-if errorlevel 1 exit /b 1
+:: Install fix-up for a non version-specific boost include
+move %LIBRARY_INC%\boost-%PKG_VERSION_MAJOR%_%PKG_VERSION_MINOR%\boost %LIBRARY_INC%
+if errorlevel 1 exit 1
+
+:: Move dll's to LIBRARY_BIN
+move %LIBRARY_LIB%\*vc%LIB_VER%-mt-%PKG_VERSION_MAJOR%_%PKG_VERSION_MINOR%.dll "%LIBRARY_BIN%"
+if errorlevel 1 exit 1
